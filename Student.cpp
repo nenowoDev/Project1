deb03cfff14f1e3a9fe3e5b48d9fab63b3929b24
raakeshUTM <128468074+raakeshUTM@users.noreply.github.com>
2023-11-27 04:36:34 +0800

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
+#include <string>
+#include "Student.h"
 using namespace std;
 
-int main () {
-    cout << "Hello";
-    return 0;
-    cout<<"aaa";
-}
+// Student :: Student(string _id, string _name, string _programEnroll, string _ic, string _address, string _gender, string _DOB, string _phoneNo) {
+//     id = _id;
+//     name = _name;
+//     programEnroll = _programEnroll;
+//     ic = _ic;
+//     address = _address;
+//     gender = _gender;
+//     DOB = _DOB;
+//     phoneNo = _phoneNo;
+// }
+
+string Student :: getID()            { return id; }
+string Student :: getName()          { return name; }
+string Student :: getProgramEnroll() { return programEnroll; }
+string Student :: getIC()            { return ic; }
+string Student :: getAddress()       { return address; }
+string Student :: getGender()        { return gender; }
+string Student :: getDOB()           { return DOB; }
+string Student :: getPhoneNo()       { return phoneNo; }
