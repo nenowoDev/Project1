6689fe5809b40c24ef3ef13ac06752399ab76498
raakeshUTM <128468074+raakeshUTM@users.noreply.github.com>
2023-11-30 01:43:43 +0800

    Update Student.h

--- Student.h
+++ Student.h
@@ -24,6 +24,7 @@ class Student {
 		string getDOB();
 		
 
+<<<<<<< HEAD
 		// string editID(string newID);
 		// string editName(string newName);
 		// string editProgramEnroll(string newProg);
@@ -32,12 +33,24 @@ class Student {
         // string editGender(string newGender);
         // string editPhoneNo(string newPhone);
 		// string editDOB(string newDOB);
+=======
+		void editID(string newID);
+		void editName(string newName);
+		void editProgramEnroll(string newProg);
+        void editIC(string newIC);
+		void editAddress(string newAddress);
+        void editGender(string newGender);
+        void editPhoneNo(string newPhone);
+		void editDOB(string newDOB);
+>>>>>>> 7b32ba4294ab4e81b17d85442f5f78830b3302e9
 
 
         friend void readFile(Student[], int&);
-        friend void displayRecord(Student [], int&);
 		friend void add(Student [], int&);
+<<<<<<< HEAD
 	    friend void del(Student [], int&);
+=======
+>>>>>>> 7b32ba4294ab4e81b17d85442f5f78830b3302e9
 };
 
 
