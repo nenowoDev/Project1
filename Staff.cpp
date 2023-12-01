aec8d89029e6764b70997d32ccbb7882218210a0
nenowo <38612806+nenowoDev@users.noreply.github.com>
2023-12-01 12:49:39 +0800

    a

--- Staff.cpp
+++ Staff.cpp
@@ -44,16 +44,7 @@ void Staff::mergeSort(Student theArray[],int first,int last){
         } 
 } 
 
-void Staff::selectionSort(int Data[], int n){
-    for (int last = n-1; last >= 1; --last){
-        int largestIndex = 0;
-        for (int p=1;p <= last; ++p){
-            if (Data[p]>Data[largestIndex])
-                largestIndex = p;
-        } 
-        swap(Data[largestIndex],Data[last]);
-    }  
-} 
+
 
 
 void Staff::edit(Student stud[],int &count,int index=-1){
@@ -250,169 +241,243 @@ void Staff::edit(Student stud[],int &count,int index=-1){
 
 
 
-void Staff::search(Student stud[],int &count){
+
+//Bubble Sort
+void Staff :: BubbleSort(Student data[], int n)
+{
+    Student tempValue;
+    bool sorted = false;
+    for(int pass = 1; (pass < n) && !sorted; ++pass) 
+    {   
+        sorted = true;
+        for(int x = 0; x < n - pass; x++)
+        {
+            if(data[x].getID() > data[x+1].getID())
+            {
+                tempValue = data[x];
+                data[x] = data[x+1];
+                data[x+1] = tempValue;
+                sorted = false;
+            }
+        }
+    }
+}
+
+//Insertion Sort
+void Staff :: InsertionSort(Student stud[], int n) {
+    Student temp;
+    int X, last;
+    for (last = n-1; last >= 1; last--)
+    {
+        X = 0;
+        for (int p = 1; p <= last; ++p) 
+        {
+            if (stud[p].getID() > stud[X].getID())
+                X = p;
+        }
+        temp = stud[last];
+        stud[last] = stud[X];
+        stud[X] = temp;
+    }
+}
+
+//Selection Sort
+void Staff :: SelectionSort(Student Data[], int n) 
+{
+    for (int last = n-1; last >= 1; --last)
+    {
+        int largestIndex = 0;
+        for (int p = 1; p <= last; ++p)
+        {
+            if (Data[p].getID() > Data[largestIndex].getID())
+                largestIndex = p;
+        }
+        swap(Data[largestIndex], Data[last]);
+    }
+}
+void swap(int& x, int& y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+// Function to read data from a file and store it in an array of Student objects
+void Staff :: readFile(Student stud[], int &count) {
+    int n = 0;
+    ifstream inp("Record.csv");
+    if (!inp) {
+         cout<<"\t\t\tFILE NOT FOUND!";
+         cout<<"\n\n";
+    }
+    else {
+        while(getline(inp, stud[n].id, ',')) {
+         	getline(inp, stud[n].name, ',');
+         	getline(inp, stud[n].programEnroll, ',');
+         	getline(inp, stud[n].ic, ',');
+         	getline(inp, stud[n].address, ',');
+			getline(inp, stud[n].gender, ',');
+            getline(inp, stud[n].DOB, ',');
+			getline(inp, stud[n].phoneNo);
+            n++;
+        }
+        count = n;
+    }
+    inp.close();
+}
+
+// Function to add student details
+void Staff :: add(Student stud[], int &count) {
+	cout<<"***********************************************************************\n";
+    cout<<"                          CREATE NEW RECORD\n";
+    cout<<"***********************************************************************\n\n";
+	cout<<"\n\t\t  -------------------------------\n";
+    cout<<"\t\t     ENTER THE STUDENT DETAILS\n";
+    cout<<"\t\t  -------------------------------\n\n";
+    cin.ignore();
+    cout<<"\t	ID NUMBER        -> "; getline(cin, stud[count].id); 
+    cout<<"\t	NAME             -> "; getline(cin, stud[count].name);  
+	cout<<"\t	PROGRAM ENROLL   -> "; getline(cin, stud[count].programEnroll); 
+	cout<<"\t	IC NUMBER        -> "; getline(cin, stud[count].ic); 
+	cout<<"\t	ADDRESS          -> "; getline(cin, stud[count].address); 
+	cout<<"\t	GENDER           -> "; getline(cin, stud[count].gender); 
+	cout<<"\t	DATE OF BIRTH    -> "; getline(cin, stud[count].DOB); 
+    cout<<"\t	PHONE NUMBER     -> "; getline(cin, stud[count].phoneNo);
+    cout<<endl<<endl;
+    count++;
+
+    ofstream f("Record.csv");
+    for(int i = 0; i < count; i++) {
+    	f << stud[i].getID() << "," 
+          << stud[i].getName() << "," 
+          << stud[i].getProgramEnroll() << "," 
+          << stud[i].getIC() << "," 
+          << stud[i].getAddress() << "," 
+          << stud[i].getGender() << "," 
+          << stud[i].getDOB() << ","   
+          << stud[i].getPhoneNo() << endl;     
+	}
+    f.close();
 
     system("cls");
     cout<<"***********************************************************************\n";
-    cout<<"                        SEARCH STUDENT RECORD\n";
+    cout<<"                          CREATE NEW RECORD\n";
     cout<<"***********************************************************************\n\n";
-    cout<<"\t\t"<<setw(25)<<left<<"[1] ALL"           <<"\n";
-    cout<<"\t\t"<<setw(25)<<left<<"[2] ID"            <<"\n";
-    cout<<"\t\t"<<setw(25)<<left<<"[3] NAME"          <<"\n";
-    cout<<"\t\t"<<setw(25)<<left<<"[4] PROGRAMME"     <<"\n";
-    cout<<"\t\t"<<setw(25)<<left<<"[5] IC NUMBER"     <<"\n";
-    cout<<"\t\t"<<setw(25)<<left<<"[6] ADDRESS"       <<"\n";
-    cout<<"\t\t"<<setw(25)<<left<<"[7] GENDER"        <<"\n";
-    cout<<"\t\t"<<setw(25)<<left<<"[8] DATE OF BIRTH" <<"\n";
-    cout<<"\t\t"<<setw(25)<<left<<"[9] PHONE NUMBER"  <<"\n";
+    cout<<"\n\n\n\t\t\tSTUDENT DETAILS RECORDED\n\n\n";	
+    char y;
+    cout<<"\t\tDo you want to sort the student list (Y/N) : ";
+    cin>>y;
+    if (y=='Y' || y=='y') {
+        InsertionSort(stud, count);
+        cout<<"\n\n\t\t\tThe student list is sorted\n\n\n";
+    } 
+    else 
+        cout<<"\n\n\t\t\tThe list is not sorted\n\n\n";
 
-    cout<<"\n\n\t\tSELECT ATTRIBUTE TO SEARCH (1-9) <<" ;
-    int searchwhere;
-    cin>>searchwhere;
-    int tempc=0;
+    system("cls");
+    displayRecord(stud, count);
+}
 
-    if(searchwhere<1||searchwhere>9){
-            search(stud,count);
+//Function to delete student details
+void Staff :: del(Student stud[], int &count) {
+    string id; int value = 0;
+	cout<<"***********************************************************************\n";
+    cout<<"                        DELETE STUDENT RECORD\n";
+    cout<<"***********************************************************************\n\n";
+    readFile(stud, count);
+    cout<<"\n\n\n\t\t ENTER STUDENT ID : ";
+    cin>>id;
+    system("cls");
+    int x=0;
+    for(int n = 0; n < count; n++)
+    	if(id == stud[n].getID()) {
+        	value=1;
+        	x=n;	
+        }
+	
+    if (value == 1) {
+		ofstream f("Record.csv");
+		for(int i = 0; i < count; i++) {
+    		if(i == x) 
+    			continue;		
+    		else{
+    		f << stud[i].getID() << "," 
+              << stud[i].getName() << "," 
+              << stud[i].getProgramEnroll() << "," 
+              << stud[i].getIC() << "," 
+              << stud[i].getAddress() << "," 
+              << stud[i].getGender() << "," 
+              << stud[i].getDOB() << ","   
+              << stud[i].getPhoneNo() << endl;  	
+			}		
+		}
+    	f.close();	
+	}
+
+    system("cls");
+    cout<<"***********************************************************************\n";
+    cout<<"                        DELETE STUDENT RECORD\n";
+    cout<<"***********************************************************************\n\n";
+    if(value==0)
+    	cout<<"\n\n\n\t\t\tRECORD NOT FOUND!\n\n\n\n\n"; 
+	else {
+        cout<<"\n\n\n\t\tTHE RECORD WAS DELETED SUCCESSFULLY\n\n\n";
+        char y;
+        cout<<"\tDo you want to sort the student list (Y/N) : ";
+        cin>>y;
+        if (y=='Y' || y=='y') {
+            readFile(stud, count);
+            BubbleSort(stud, count); 
+            cout<<"\n\n\t\tThe student list is sorted\n\n\n";
+        } 
+        else {
+            readFile(stud, count);
+            cout<<"\n\n\t\t\tThe list is not sorted\n\n\n";	
+        }     
     }
 
-    
-    else{
-
-        cout<<"\n\t\tVALUES TO SEARCH:";
-        cin.ignore();
-        string searchwhat;
-        getline(cin,searchwhat);
-        Student temp[count];
-        if(searchwhere==1){
-            for(int i=0;i<count;i++){
-                
-                if(stud[i].getID().find(searchwhat)!=string::npos){
-                    temp[tempc]=stud[i];
-                    tempc++;
-                }
-                else if(stud[i].getName().find(searchwhat)!=string::npos){
-                    temp[tempc]=stud[i];
-                    tempc++;
-                }
-                else if(stud[i].getProgramEnroll().find(searchwhat)!=string::npos){
-                    temp[tempc]=stud[i];
-                    tempc++;
-                }
-                else if(stud[i].getIC().find(searchwhat)!=string::npos){
-                    temp[tempc]=stud[i];
-                    tempc++;
-                }
-                else if(stud[i].getAddress().find(searchwhat)!=string::npos){
-                    temp[tempc]=stud[i];
-                    tempc++;
-                }
-                else if(stud[i].getGender().find(searchwhat)!=string::npos){
-                    temp[tempc]=stud[i];
-                    tempc++;
-                }
-                else if(stud[i].getDOB().find(searchwhat)!=string::npos){
-                    temp[tempc]=stud[i];
-                    tempc++;
-                }
-                else if(stud[i].getPhoneNo().find(searchwhat)!=string::npos){
-                    temp[tempc]=stud[i];
-                    tempc++;
-                }
-            }
-
-        }
-
-        else if(searchwhere==2){
-            for(int i=0;i<count;i++){
-                
-                if(stud[i].getID().find(searchwhat)!=string::npos){
-                    temp[tempc]=stud[i];
-                    tempc++;
-                }
-            }
-        }
-
-        else if(searchwhere==3){
-            for(int i=0;i<count;i++){
-                
-                if(stud[i].getName().find(searchwhat)!=string::npos){
-                    temp[tempc]=stud[i];
-                    tempc++;
-                }
-            }
-        }
-
-
-        else if(searchwhere==4){
-            for(int i=0;i<count;i++){
-                
-                if(stud[i].getProgramEnroll().find(searchwhat)!=string::npos){
-                    temp[tempc]=stud[i];
-                    tempc++;
-                }
-            }
-        }
-
-        else if(searchwhere==5){
-            for(int i=0;i<count;i++){
-                
-                if(stud[i].getIC().find(searchwhat)!=string::npos){
-                    temp[tempc]=stud[i];
-                    tempc++;
-                }
-            }
-        }
-
-        else if(searchwhere==6){
-            for(int i=0;i<count;i++){
-                
-                if(stud[i].getAddress().find(searchwhat)!=string::npos){
-                    temp[tempc]=stud[i];
-                    tempc++;
-                }
-            }
-        }
-
-        else if(searchwhere==7){
-            for(int i=0;i<count;i++){
-                
-                if(stud[i].getGender().find(searchwhat)!=string::npos){
-                    temp[tempc]=stud[i];
-                    tempc++;
-                }
-            }
-        }
-
-        else if(searchwhere==8){
-            for(int i=0;i<count;i++){
-                
-                if(stud[i].getDOB().find(searchwhat)!=string::npos){
-                    temp[tempc]=stud[i];
-                    tempc++;
-                }
-            }
-        }
-
-        else if(searchwhere==9){
-            for(int i=0;i<count;i++){
-                
-                if(stud[i].getPhoneNo().find(searchwhat)!=string::npos){
-                    temp[tempc]=stud[i];
-                    tempc++;
-                }
-            }
-        }
-
-
-        
-
-
-        system("cls");
-        cout<<"\n\n\t\tOCCURENCE OF \""<<searchwhat<<"\"\n";
-        stud->displayRecord(temp,tempc);
-
-
-    }   
-
-
+    system("cls");
+    displayRecord(stud, count);
 }
+ 
+// Function to display all record of student in an array for the staff to see
+void Staff :: displayRecord(Student stud[], int &count) {
+    cout<<"*************************************************************************************************************************************\n";
+    cout<<"                                                       DISPLAY ALL RECORD\n";
+    cout<<"*************************************************************************************************************************************\n\n";
+    cout<<"ID        NAME                PROGRAM ENROLL              IC NUMBER        ADDRESS           GENDER    DATE OF BIRTH     PHONE NUMBER\n";
+    cout<<"-------------------------------------------------------------------------------------------------------------------------------------\n";
+    for (int i = 0; i < count; i++) {
+        cout<<left<<setw(10)<<stud[i].getID()
+            <<setw(20)<<stud[i].getName()
+            <<setw(28)<<stud[i].getProgramEnroll()
+            <<setw(17)<<stud[i].getIC()
+            <<setw(18)<<stud[i].getAddress()
+            <<setw(10)<<stud[i].getGender()
+            <<setw(18)<<stud[i].getDOB()               
+            <<setw(15)<<stud[i].getPhoneNo()<<endl;
+    }
+    cout<<endl<<endl;
+}
+
+// Function to display sorted record
+void Staff :: display(Student stud[], int &count) {
+    displayRecord(stud, count);
+    system("PAUSE");
+    system("cls");
+    char y;
+    cout<<"*************************************************************************************************************************************\n";
+    cout<<"                                                       DISPLAY ALL RECORD\n";
+    cout<<"*************************************************************************************************************************************\n\n";
+    cout<<"\n\n\n\t\tDo you want to sort the student list (Y/N) : ";
+    cin>>y;
+    if (y=='Y' || y=='y') {
+        SelectionSort(stud, count);
+        cout<<"\n\n\t\t\tThe student list is sorted\n\n\n";
+        system("cls");
+        displayRecord(stud, count);
+    } 
+    else 
+        cout<<"\n\n\t\t\tThe list is not sorted\n\n\n";
+}
+
+
--- Staff.h
+++ Staff.h
@@ -9,9 +9,21 @@ class Staff {
     public:
         void merge(Student theArray[], int first, int mid, int last);
         void mergeSort(Student theArray[],int first,int last);
-        void selectionSort(int Data[], int n);
+        
         void edit(Student stud[],int &count,int index);
         void search(Student stud[],int &count);
+
+        void InsertionSort(Student[], int);
+		void BubbleSort(Student[], int);
+		void SelectionSort(Student[], int);
+
+        void readFile(Student[], int&);
+		void add(Student [], int&);
+	    void del(Student [], int&);
+		void displayRecord(Student [], int&);
+		void display(Student[], int&);
+
+        friend class Student;
 };
 
 
