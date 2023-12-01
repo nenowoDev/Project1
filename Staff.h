58c1c0aa8e142039cea3b4fe04bae80b6426ea85
raakeshUTM <128468074+raakeshUTM@users.noreply.github.com>
2023-12-01 18:40:00 +0800

    Update Staff.h

--- Staff.h
+++ Staff.h
@@ -4,23 +4,18 @@
 #include "Student.h"
 
 class Staff {
-    private :
-
     public:
         void merge(Student theArray[], int first, int mid, int last);
         void mergeSort(Student theArray[],int first,int last);
-        
-        void edit(Student stud[],int &count,int index);
-        void search(Student stud[],int &count);
-
         void InsertionSort(Student[], int);
 		void BubbleSort(Student[], int);
 		void SelectionSort(Student[], int);
 
+        void edit(Student stud[],int &count,int index);
         void readFile(Student[], int&);
 		void add(Student [], int&);
 	    void del(Student [], int&);
-		void displayRecord(Student [], int&);
+		//void displayRecord(Student [], int&);
 		void display(Student[], int&);
 
         friend class Student;
